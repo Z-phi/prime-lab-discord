7536ae86f5b80948623c1e8c665754357cba7c4b
Sam Starke <zetasm0@gmail.com>
2021-09-14 18:38:21 -0500

    setup

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { Client, Intents } from 'discord.js';
+import * as fs from 'fs';
+
+const token = fs.readFileSync('token.txt').toString();
+console.log(token);
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+    console.log('ready!');
+});
+
+client.on('message', m => {
+    if (m.author.bot) return;
+
+    m.reply('care?');
+});
+
+client.login(token);
